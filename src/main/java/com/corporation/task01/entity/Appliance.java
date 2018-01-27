4d302015989446014a9badb3775e3c3e6eb7e70e
louchayaN <35494740+louchayaN@users.noreply.github.com>
2018-01-27 11:13:11 +0300

    delete

--- src/main/java/com/corporation/task01/entity/Appliance.java
+++
@@ -1,5 +0,0 @@
-package com.corporation.task01.entity;
-
-public class Appliance {
-
-}
--- src/main/java/com/corporation/task01/entity/criteria/ApplianceType.java
+++
@@ -1,6 +0,0 @@
-package com.corporation.task01.entity.criteria;
-
-public enum ApplianceType {
-	
-	OVEN, LAPTOP, REFRIGERATOR, VACUUMCLEANER, TABLETPC, SPEAKERS 
-}
